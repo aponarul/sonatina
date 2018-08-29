e1774d61899fd28f090a542482d96f2318e31fcf
PonArul <gh0ipon@ent.rt.csaa.com>
2018-08-29 14:37:49 -0700

    updated python source code

--- python/prep.py
+++ python/prep.py
@@ -3,6 +3,6 @@ from datetime import date, timedelta
 import os
 yesterday = datetime.date.today() - timedelta(1)
 command = "python /d2/data/apps/ubi/ingestion/octo/scripts/prepDynamic.py %s" % yesterday
-print(command)
-#os.system(command)
+#print(command)
+os.system(command)
 
